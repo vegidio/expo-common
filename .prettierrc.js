f9031f389572ca5d09fc31b9e8d6e508ce146ae3
Vinicius Egidio <me@vinicius.io>
2024-10-15 05:12:55 +0200

    chore: initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: false,
+    editorconfig: true,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'none',
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# expo-common
+
+## Configuration
+
+- Install Prettier: `expo install -- --save-dev prettier eslint-config-prettier eslint-plugin-prettier`
